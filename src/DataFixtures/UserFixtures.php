b55970bada3c0c3d3921903aa8b958216aaae91a
Mohamed <mohamed-siso@hotmail.fr>
2020-10-22 12:39:36 +0200

    connexion/deconnexion

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+
+
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $user = new User();
+        $user->setAdmin(1);
+        $user->setEmail("demo@demo.fr");
+        $user->setUsername("demo");
+        $user->setNom("demonstration");
+        $user->setPassword($this->encoder->encodePassword($user,"demo"));
+        $manager->persist($user);
         $manager->flush();
     }
+
 }
